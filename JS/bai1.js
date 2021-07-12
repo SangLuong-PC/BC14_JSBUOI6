dad891f247c4b730dd41f898bb0cbf718ee3a385
SangLuong-PC <sang.khanhhn@gmail.com>
2021-07-12 18:56:45 +0700

    add

---
+++ JS/bai1.js
@@ -0,0 +1,20 @@
+function getResult() {
+    var i = 0;
+    var sum = 0;
+    while (sum <= 10000) {
+      i++;
+      sum += i;
+    }
+    return i;
+  }
+  
+  function showResult() {
+    var result = getResult();
+    var showResult = getMyEl("showResult");
+    var tagP = document.createElement("p");
+    tagP.className = "p-3 bg-info rounded";
+    tagP.innerHTML = "Số n nguyên dương nhỏ nhất thỏa điều kiện 1 + 2 + ... + n > 10000 là: " + result;
+    showResult.appendChild(tagP);
+  }
+  
+  showResult();
